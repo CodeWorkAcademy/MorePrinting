e488ff3ce787426380054d8d693ccd02abb4b4d5
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-23 13:42:54 -0400

    Commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("+--------------------------------------+");
+        System.out.println("|       Java Bank Atn Receipt          |");
+        System.out.println("|   Wednesday, December 2, 2015        |");
+        System.out.println("|   Atm Location  #123                 |");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |");
+        System.out.println("|       Account Number:   1234567      |");
+        System.out.println("|       Customer:   John Q. Public     |");
+        System.out.println("|       Transaction type:  Deposit     |");
+        System.out.println("|       Transaction Amount: $500.00    |");
+        System.out.println("|       Account Balance: $1,500.00     |");
+        System.out.println("|                                      |");
+        System.out.println("|       Thank you for banking with us  |");
+        System.out.println("|             Have a coffee day        |");
+        System.out.println("|                                      |");
+        System.out.println("+--------------------------------------+");
+
     }
 }
